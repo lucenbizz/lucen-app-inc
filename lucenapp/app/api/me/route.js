7bd4a1248d91b20166550c71ef2983a0734c67a0
lucenbizz <zayhubbard4@yahoo.com>
2025-09-03 15:02:52 -0400

    Create route.js

---
+++ lucenapp/app/api/me/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server';
+import { getUserAndProfile } from '../../lib/supabaseServerClient.js';
+
+export async function GET() {
+  const { user, profile } = await getUserAndProfile();
+  return NextResponse.json({ user, profile });
+}
