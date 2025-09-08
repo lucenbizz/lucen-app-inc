358ec2e9af6e0ed5391eb3bae3bda15b4cbfc740
lucenbizz <zayhubbard4@yahoo.com>
2025-09-07 21:56:11 -0400

    Update layout.jsx

--- lucenapp/app/layout.jsx
+++ lucenapp/app/layout.jsx
@@ -39,7 +39,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0a0a0a" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
